b42cfefcfb339df5c04b6c422602ef1a05a73b21
MaxmilliamOkafor <71933419+MaxmilliamOkafor@users.noreply.github.com>
2026-04-02 22:25:40 +0100

    Add files via upload

---
+++ Download Apollo.io Free B2B Phone Number & Email Finder/serviceWorker.js
@@ -0,0 +1,10 @@
+// eslint-strict
+// eslint-disable-next-line leadgenie/no-import-extensions, leadgenie/imports-without-specifiers -- The file doesn't exist yet. It will be created by the build.
+import './js/background.bundle.js';
+
+/**
+ * ! DO NOT CHANGE
+ *
+ * This file will be copied to the root folder of the extension build
+ *  which is a requirement for using service workers with manifest
+ */
